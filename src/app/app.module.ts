7e2513e05c9f9e16765f4fbd5311dd2f90b20c42
Bogdan Tushevskyi <tushevskuy@gmail.com>
2017-09-18 22:00:34 +0300

    add reactive form and angular animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,24 @@
-import { NgbModule } 	       from '@ng-bootstrap/ng-bootstrap';
-import { BrowserModule }       from '@angular/platform-browser';
-import { NgModule } 		   from '@angular/core';
-import { HttpClientModule }    from '@angular/common/http';
-import { FormsModule } 		   from '@angular/forms';
-import { TextMaskModule } 	   from 'angular2-text-mask';
+import { NgbModule } 	           from '@ng-bootstrap/ng-bootstrap';
+import { BrowserModule }           from '@angular/platform-browser';
+import { NgModule } 		       from '@angular/core';
+import { HttpClientModule }        from '@angular/common/http';
+import { FormsModule } 		       from '@angular/forms';
+import { ReactiveFormsModule }     from '@angular/forms';
+import { TextMaskModule } 	   	   from 'angular2-text-mask';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
-import { AppComponent }        from './app.component';
-import { PriceComponent }      from './price/price.component';
-import { EmailSendComponent }  from './email-send/email-send.component';
-import { PriceModalComponent } from './price-modal/price-modal.component';
-import { HeaderComponent }     from './header/header.component';
-import { SmsSendComponent }    from './sms-send/sms-send.component';
-import { HeroFormComponent } from './hero-form/hero-form.component';
+import { AppComponent }            from './app.component';
+import { PriceComponent }          from './price/price.component';
+import { EmailSendComponent }      from './email-send/email-send.component';
+import { PriceModalComponent }     from './price-modal/price-modal.component';
+import { HeaderComponent }         from './header/header.component';
+import { SmsSendComponent }        from './sms-send/sms-send.component';
+import { HeroFormComponent }       from './hero-form/hero-form.component';
 
 @NgModule({
   declarations: [AppComponent, PriceComponent, EmailSendComponent, PriceModalComponent, HeaderComponent, SmsSendComponent, HeroFormComponent],
-  imports: [BrowserModule, HttpClientModule, NgbModule.forRoot(), TextMaskModule, FormsModule],
+  imports: [BrowserModule, HttpClientModule, NgbModule.forRoot(), TextMaskModule, FormsModule, ReactiveFormsModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/sms-send/sms-send.component.ts
+++ src/app/sms-send/sms-send.component.ts
@@ -1,20 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { SmsSendService }	          from './sms-send.service';
+import { Component, OnInit }                          from '@angular/core';
+import { SmsSendService }	                            from './sms-send.service';
+import { FormControl, FormGroup }                     from '@angular/forms';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-sms-send',
   templateUrl: './sms-send.component.html',
   styleUrls: ['./sms-send.component.sass'],
-  providers: [ SmsSendService ]
+  providers: [ SmsSendService ],
+  animations: [
+    trigger('hideElement', [
+      state('inactive', style({
+        opacity: 1
+      })),
+      state('active', style({
+        opacity: 0
+      })),
+      transition('inactive => active', animate('200ms ease-in')),
+      transition('active => inactive', animate('200ms ease-out'))
+    ])
+  ]
 })
 
 export class SmsSendComponent implements OnInit {
 
   info_blocks: Array<object>;
+  smsSendGroup: any;
   smssend: object;
   response: object;
   promoCode: number; 
   successStatus: boolean;
+  state: string = 'inactive';
+  animationDoneResult: boolean = true;
   mask: any[] = ['+', '3', '8', ' ', '(', /[0-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
 
   constructor(private service: SmsSendService) {
@@ -34,12 +51,25 @@ export class SmsSendComponent implements OnInit {
         title: "Придите и <br> получите чистку <br> бесплатно",
         term: "В любом из отделений <br> Vesch с 16 октября по <br> 30 ноября"
       }
-    ]
+    ],
+
+    this.smsSendGroup = new FormGroup({
+       inputPhone: new FormControl()
+    });
   }
 
   ngOnInit() {}
+
+  hideElementAnimation(): void {
+    this.state = (this.state === 'inactive' ? 'active' : 'inactive');
+  }
+
+  animationDone(e): void {
+    console.log(e);
+    this.animationDoneResult = false;
+  }
  
-  smsSend(user_phone): void {
+  onSubmit(user_phone): void {
   	this.smssend = this.service.smsSendRequest(user_phone)
   	  .subscribe( data => { 
         this.response = data;
