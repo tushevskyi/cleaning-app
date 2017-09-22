462b8e34658c752235d1393cc22e447005f7c07a
Bogdan Tushevskyi <tushevskuy@gmail.com>
2017-09-22 13:50:32 +0300

    add animation for price component

--- src/app/price/price.component.ts
+++ src/app/price/price.component.ts
@@ -15,6 +15,16 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
       })),
       transition('inactive => active', animate('200ms ease-in')),
       transition('active => inactive', animate('200ms ease-in'))
+    ]),
+    trigger('hoverAnimationWave', [
+      state('inactive', style({
+        opacity: '0'
+      })),
+      state('active', style({
+        opacity: '1'
+      })),
+      transition('inactive => active', animate('250ms ease')),
+      transition('active => inactive', animate('250ms ease'))
     ])
   ]
 })
