a662e1a425745f00d8ea9c22d508256d7a55b737
Bogdan Tushevskyi <tushevskuy@gmail.com>
2017-10-25 16:56:05 +0300

    add change from client (add telegram icon and change text in action block)

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -8,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 export class FooterComponent implements OnInit {
 
   footerLegal: string;	
+  icons: Array<object>;
 
-  constructor() { 
-  	this.footerLegal = "footer.legal_text"
+  constructor() {
+  	this.footerLegal = "footer.legal_text";
+  	this.icons = [
+  		{
+  			class: 'icon_fb',
+  			href: 'https://www.facebook.com/vesch.ua/',
+  			src: '../assets/svg/fb.svg',
+  			alt: 'facebook icon'
+  		},
+  		{
+  			class: 'icon_inst',
+  			href: 'https://www.instagram.com/vesch.ua/',
+  			src: '../assets/svg/inst.svg',
+  			alt: 'instagram icon'
+  		},
+  		{
+  			class: 'icon_tm',
+  			href: 'https://t.me/Vesch_bot',
+  			src: '../assets/svg/tm.svg',
+  			alt: 'telegram icon'
+  		}
+  	]
   }
 
   ngOnInit() {
